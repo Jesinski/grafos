396809eedcb1c4412e14f5c857d04cd896f1ab1a
Marcelo Cohen <mflashbr@gmail.com>
2020-10-07 20:30:51 -0300

    Implementação de caminhamento em largura (BFS)

--- BasicGraphs/src/TesteGrafo.java
+++ BasicGraphs/src/TesteGrafo.java
@@ -4,10 +4,14 @@ public class TesteGrafo {
          * Graph g = new Graph(5); g.addEdge(1,2); g.addEdge(0,1); g.addEdge(2,4);
          * g.addEdge(4,3); g.addEdge(1,4);
          */
-        Graph g = new Graph(4);
-        g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(0, 3);
+
+        /*
+         * Graph g = new Graph(4); g.addEdge(0, 1); g.addEdge(1, 2); g.addEdge(0, 3);
+         * g.addEdge(0, 2);
+         */
+
+        In in = new In("../tinyG.txt");
+        Graph g = new Graph(in);
 
         System.out.println("Adjacentes ao 1:");
         for (int v : g.adj(1)) {
@@ -16,12 +20,12 @@ public class TesteGrafo {
 
         System.out.println(g.toDot());
 
-        CaminhamentoProfundidade dfs = new CaminhamentoProfundidade(g, 0);
-        dfs.exibe();
-        System.out.println();
-        for (int v = 0; v < g.V(); v++) {
-            if (dfs.hasPath(v))
-                System.out.println(v + ":" + dfs.pathTo(v));
-        }
+        /*
+         * CaminhamentoProfundidade dfs = new CaminhamentoProfundidade(g, 0);
+         * dfs.exibe(); System.out.println(); for (int v = 0; v < g.V(); v++) { if
+         * (dfs.hasPath(v)) System.out.println(v + ":" + dfs.pathTo(v)); }
+         */
+        CaminhamentoLargura bfs = new CaminhamentoLargura(g, 0);
+        bfs.exibe();
     }
 }
